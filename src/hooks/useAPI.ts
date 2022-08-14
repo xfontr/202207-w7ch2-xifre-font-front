d859e304d7140d13e5c8853d37cb2fc7e8bd6114
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-14 13:22:21 +0200

    Created deleteRobot function inside hook

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -1,6 +1,10 @@
 import { useDispatch } from "react-redux";
 import { useCallback } from "react";
-import { getRobotsActionNew } from "../store/actionCreators/actionCreators";
+import {
+  deleteRobotActionNew,
+  getRobotsActionNew,
+} from "../store/actionCreators/actionCreators";
+import IRobot from "../store/types/interfaces";
 
 const useAPI = () => {
   const url = process.env.REACT_APP_API_URL;
@@ -12,8 +16,18 @@ const useAPI = () => {
     dispatch(getRobotsActionNew(response));
   }, [dispatch, url]);
 
+  const deleteRobot = useCallback(
+    async (robot: IRobot) => {
+      await fetch(`${url}robots/delete/${robot._id}`, {
+        method: "DELETE",
+      });
+      await dispatch(deleteRobotActionNew(robot));
+    },
+    [dispatch, url]
+  );
   return {
     getAllRobots,
+    deleteRobot,
   };
 };
 
