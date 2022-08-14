71584664c60b22883f33cfea0080c2ff7b23259d
Xifre Font <xifre.font.97@gmail.com>
2022-08-14 12:19:40 +0200

    Tested homepage

---
+++ src/pages/HomePage.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import HomePage from "./HomePage";
+
+describe("Given a HomPage component", () => {
+  describe("When instantiated", () => {
+    test("Then it should show a list of robots", () => {
+      render(
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      );
+
+      const initialRobotName = "Bender";
+      const robotHeading = screen.getByRole("heading", {
+        name: initialRobotName,
+      });
+
+      expect(robotHeading).toBeInTheDocument();
+    });
+  });
+});
