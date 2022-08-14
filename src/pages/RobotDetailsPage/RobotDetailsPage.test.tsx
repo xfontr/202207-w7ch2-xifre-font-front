8c06a7c19012f7a968f4efdf42ede86e51b1825b
Xifre Font <xifre.font.97@gmail.com>
2022-08-14 12:42:23 +0200

    Tested robotDetailsPage component

---
+++ src/pages/RobotDetailsPage/RobotDetailsPage.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../app/store";
+import RobotDetailsPage from "./RobotDetailsPage";
+
+describe("Given a RobotDetails component", () => {
+  describe("When instantiated", () => {
+    test("Then it should show a component with all the robot details", () => {
+      const dummyRobot = {
+        name: "Bender",
+        image:
+          "https://www.paredro.com/wp-content/uploads/2014/05/sorayama-sexy-robot-2.jpg",
+        creationDate: "13/08/2022",
+        speed: 9,
+        endurance: 3,
+      };
+
+      render(
+        <Provider store={store}>
+          <RobotDetailsPage />
+        </Provider>
+      );
+
+      const robotName = screen.getByRole("heading", { name: dummyRobot.name });
+
+      expect(robotName).toBeInTheDocument();
+    });
+  });
+});
