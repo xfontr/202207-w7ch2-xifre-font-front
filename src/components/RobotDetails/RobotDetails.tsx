5f76ec983f2e7de15a7ffd4698f03ca25b5e8168
Xifre Font <xifre.font.97@gmail.com>
2022-08-14 12:29:01 +0200

    Robot details skeleton

---
+++ src/components/RobotDetails/RobotDetails.tsx
@@ -0,0 +1,32 @@
+import IRobot from "../../store/types/interfaces";
+
+interface RobotDetailsProps {
+  robot: IRobot;
+}
+
+const RobotDetails = ({ robot }: RobotDetailsProps): JSX.Element => {
+  return (
+    <article>
+      <h2 className="robot__name">{robot.name}</h2>
+      <span className="robot__date">Created on {robot.creationDate}</span>
+      <img
+        className="robot__image"
+        height="350"
+        width="320"
+        src={robot.image}
+        alt={robot.name}
+      />
+      <ul className="robot__data">
+        <li className="robot__data-item">
+          <span>Speed:</span>
+          {robot.speed}
+        </li>
+        <li className="robot__data-item">
+          <span>Endurance:</span> {robot.endurance}
+        </li>
+      </ul>
+    </article>
+  );
+};
+
+export default RobotDetails;
