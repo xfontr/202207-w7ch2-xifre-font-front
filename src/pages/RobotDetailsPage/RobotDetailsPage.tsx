6efd63d2c166d2dc05c6c44b620ddd7890d27aec
Xifre Font <xifre.font.97@gmail.com>
2022-08-16 00:02:24 +0200

    Fixed the infinte loop

--- src/pages/RobotDetailsPage/RobotDetailsPage.tsx
+++ src/pages/RobotDetailsPage/RobotDetailsPage.tsx
@@ -1,21 +1,33 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import RobotDetails from "../../components/RobotDetails/RobotDetails";
 import useAPI from "../../hooks/useAPI";
 import { selectAllRobots } from "../../store/selectors/selectors";
+import IRobot from "../../store/types/interfaces";
 
 const RobotDetailsPage = (): JSX.Element => {
   const { getRobotById } = useAPI();
   const { id } = useParams();
 
+  const getRobot = useCallback(async () => {
+    await getRobotById(id as string);
+  }, [id, getRobotById]);
+
   useEffect(() => {
-    getRobotById(id as string);
-  });
+    (async () => {
+      await getRobot();
+    })();
+  }, [getRobot]);
 
   const { robots } = useSelector(selectAllRobots);
 
-  return <RobotDetails robot={robots[0]} />;
+  return (
+    <>
+      {robots[0] && <RobotDetails robot={robots[0] as IRobot} />}
+      {!robots[0] && <p>Couldn't get the beep bop boy</p>}
+    </>
+  );
 };
 
 export default RobotDetailsPage;
